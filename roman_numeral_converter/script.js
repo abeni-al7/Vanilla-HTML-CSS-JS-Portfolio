7661fd84187ee6cb605c8d60624648d87f06c314
abeni-al7 <abenezeralebachew3@gmail.com>
2024-07-13 22:29:20 +0300

    Build a roman numeral converter

---
+++ roman_numeral_converter/script.js
@@ -0,0 +1,30 @@
+const numberInput = document.getElementById("number");
+const convertBtn = document.getElementById("convert-btn");
+const output = document.getElementById("output");
+
+const convertToRoman = (number) => {
+  const conversion = [[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']];
+
+  result = "";
+  for (i = 0; i < conversion.length; i++) {
+    while (conversion[i][0] <= number) {
+      number -= conversion[i][0];
+      result += conversion[i][1];
+    }
+  }
+  return result;
+}
+
+const validateInput = () => {
+  const number = numberInput.value;
+  if (number === "") {
+    output.innerText = "Please enter a valid number";
+  } else if (number < 0) {
+    output.innerText = "Please enter a number greater than or equal to 1";
+  } else if (number >= 4000) {
+    output.innerText = "Please enter a number less than or equal to 3999";
+  } else {
+    output.innerText = convertToRoman(number);
+  }
+}
+convertBtn.addEventListener("click", validateInput);
